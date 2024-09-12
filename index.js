f3d08934b92c6e96afadc950b4e240217baecc0a
abel.dieguez <abel.dieguez@wakasolutions.com>
2024-09-12 07:30:59 +0000

    cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import crypto from 'crypto';
 import fs from 'fs/promises';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 //____________________________________________________________________________
 // FILE:
